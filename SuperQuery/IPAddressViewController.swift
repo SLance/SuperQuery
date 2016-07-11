1f8d91db1f75e1fbab930b24a57a0421dfdf2c01
S.Lance <lanchao0713@msn.com>
2016-07-11 17:05:42 +0800

    接入验证IP地址API的返回数据

--- SuperQuery/IPAddressViewController.swift
+++ SuperQuery/IPAddressViewController.swift
@@ -11,6 +11,8 @@ import Alamofire
 
 class IPAddressViewController: UIViewController {
 
+    let ipRegex = "^(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])$"
+    
     @IBOutlet weak var ipAddressTextField: UITextField!
     @IBOutlet weak var resultTextView: UITextView!
     
@@ -37,7 +39,30 @@ class IPAddressViewController: UIViewController {
     */
     
     @IBAction func checkButton_touch(sender: AnyObject) {
-        
+        let ip = self.ipAddressTextField.text!
+        if ip == "" {
+            return;
+        }
+        let match = ip.rangeOfString(ipRegex, options: .RegularExpressionSearch)
+        if match == nil {
+            self.resultTextView.text = "错误的IP地址"
+            return;
+        }
+        Alamofire.request(.POST,
+            "https://apis.juhe.cn/ip/ip2addr",
+            parameters: [ "key" : "8c196438d0a6628c68700e8c28ed879b", "ip" : ip ])
+            .validate(contentType: ["application/json"])
+            .responseJSON { (response) in
+                switch response.result {
+                case .Success:
+                    if response.result.value!["result"]! != nil {
+                        let result: [String: AnyObject] = response.result.value!["result"] as! [String: AnyObject]
+                        self.resultTextView.text = "区域：\(result["area"]!)\n位置：\(result["location"]!)"
+                    }
+                case .Failure(let error):
+                    print(error)
+                }
+        }
     }
 
 }
